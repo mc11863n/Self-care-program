726a0bdbb70eeed0602d38281b5ae5a490175894
mmc11863n <melanic72007@gmail.com>
2025-12-10 12:06:45 -0500

    lab classes

--- Care.py
+++ Care.py
@@ -1,11 +1,16 @@
 class Skin:
     def __init__(self,skin_type = None,oily= None,dry = None ):
-        self.skin_type = skin_type
+        self.skin = skin_type
         self.oily = oily
         self.dry = dry
 
-    def get_skin(self,skin_type):
-        return self.skin_type
+    def get_skin(self):
+        if self.skin == self.dry:
+            return self.dry
+        elif self.skin == self.oily:
+            return self.oily
+        else:
+            return "No skin type detected"
         
 class Acne:
     def __init__(self,name,large_pores = None, small_bumps = None, zits = None):
@@ -13,21 +18,57 @@ class Acne:
         self.large_pores = large_pores
         self.small_bumps = small_bumps
         self.zits = zits
-    def get_acne_check(self):
-        return self.name
-        
-    
-class SkinCare(Skin):
 
+    def get_acne_check(self):
+        if self.name == self.large_pores:
+            return "Large pores detected"
+        elif self.name == self.small_bumps:
+            return "Small bumps detected"
+        elif self.name == self.zits:
+            return "Zits detected"
+        else:
+            return "No acne issues"
+
+
+class Budget:
+    def __init__(self,price_of_product, max_budget, min_budeget, budget):
+        self.price_of_product = price_of_product
+        self.max_budget = max_budget
+        self.min_budget = min_budeget
+        self.budget = budget
+    
+    def get_price(self):
+        return self.price_of_product
+    
+    def subtracting_price_budget(self):
+        return self.budget - self.price_of_product
+    
+    def get_budget(self):
+        if self.budget > self.max_budget:
+            return "You have exeded your budget"
+        elif self.budget < self.min_budget:
+            return "You still have ${self.budget} left"
+        else:
+            return "Your budget is at its limit"
+        
+    def __str__():
+        return
+
+class SkinCare(Skin,Acne):
     def __init__(self,product_name):
-        super().__init__(None,None,None)
         self.productname = product_name
+        Skin.__init__(skin_type= '', oily= '', dry= '')
+        Acne().__init__(name = '', large_pores = '', small_bumps = '', zits = '')
+        
 
 
 
 
 def main():
-    return
+    skin = Skin(skin_type = '', oily= ' ', dry= '')
+    acne = Acne(name = '', large_pores= '', small_bumps= '', zits= '')
+    print(skin.get_skin())
+    print(acne.get_acne_check())
 
 
 
